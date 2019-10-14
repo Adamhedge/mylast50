3d79e9ab614664776bb6f93723ffbde17f0126f5
Adamhedge <adamhedge@gmail.com>
2019-10-14 14:19:28 -0700

    (feat) #3 Button styling and animation for mobile

--- client/source/components/create_playlist.js
+++ client/source/components/create_playlist.js
@@ -17,7 +17,7 @@ class CreatePlaylist extends Component {
             <div className='fiftyButton'>
                 <p className='instructions'>Tap for your last 50 songs</p>
                 <a href="/login">
-                    <button className='button' onClick={this.handleClick}>
+                    <button className='button' onClick={this.handleClick} onTouchStart>
                         50
                     </button>
                 </a>
